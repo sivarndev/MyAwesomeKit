519766ea4fed460160ce8e7dea14d0eee031c715
Siva <siva.ping.here@gmail.com>
2024-12-07 19:48:58 +0530

    updated workflow added xcframework

---
+++ output/MyAwesomeKit.xcframework/ios-arm64/MyAwesomeKit.framework/Headers/MyAwesomeKit.h
@@ -0,0 +1,18 @@
+//
+//  MyAwesomeKit.h
+//  MyAwesomeKit
+//
+//  Created by Sivasubramaniam on 04/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyAwesomeKit.
+FOUNDATION_EXPORT double MyAwesomeKitVersionNumber;
+
+//! Project version string for MyAwesomeKit.
+FOUNDATION_EXPORT const unsigned char MyAwesomeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyAwesomeKit/PublicHeader.h>
+
+
