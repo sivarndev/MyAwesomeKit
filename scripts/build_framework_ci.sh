012eaefe87eaba7b55fec2ca9168e56efe7c3c84
Siva <siva.ping.here@gmail.com>
2024-12-09 22:07:19 +0530

    updated workflow and added build-script for framework

---
+++ scripts/build_framework_ci.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+FRAMEWORK_NAME="MyAwesomeKit"
+OUTPUT_DIR="output"
+ARCHIVES_DIR="archives"
+
+rm -rf "${ARCHIVES_DIR}" "${OUTPUT_DIR}"
+mkdir -p "${ARCHIVES_DIR}" "${OUTPUT_DIR}"
+
+echo "🏗 Building ${FRAMEWORK_NAME} for iOS Simulator..."
+xcodebuild archive \
+-workspace ${FRAMEWORK_NAME}.xcworkspace \
+-scheme ${FRAMEWORK_NAME} \
+-archivePath ${ARCHIVES_DIR}/simulator.xcarchive \
+-sdk iphonesimulator \
+SKIP_INSTALL=NO \
+BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+echo "🏗 Building ${FRAMEWORK_NAME} for iOS Devices..."
+xcodebuild archive \
+-workspace ${FRAMEWORK_NAME}.xcworkspace \
+-scheme ${FRAMEWORK_NAME} \
+-archivePath ${ARCHIVES_DIR}/ios.xcarchive \
+-sdk iphoneos \
+SKIP_INSTALL=NO \
+BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+echo "🔨 Creating XCFramework..."
+xcodebuild -create-xcframework \
+-framework ${ARCHIVES_DIR}/simulator.xcarchive/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+-framework ${ARCHIVES_DIR}/ios.xcarchive/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+-output ${OUTPUT_DIR}/${FRAMEWORK_NAME}.xcframework
+
+echo "✅ XCFramework created successfully at ${OUTPUT_DIR}/${FRAMEWORK_NAME}.xcframework"
